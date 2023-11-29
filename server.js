0cd6b590900f365752847c5242ee80b6e8083ad1
Greg Mattia <gjmattia@gmail.com>
2023-11-29 18:03:11 -0500

    added live server links

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ app.use('/friendlist', ensureLoggedIn, friendListRouter);
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://ghotel-client.onrender.com',
+        // origin: 'http://localhost:5173',
         methods: ['GET', 'POST']
     },
 });
